66f49b9f0326323cb3b7730f5031dff27fee2a32
bobchengbin <bob@phpor.me>
2015-01-05 02:08:21 +0800

    add after composer tests bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
 
-require(dirname(__DIR__).'/vendor/autoload.php');
+if(file_exists(dirname(__DIR__).'/vendor/autoload.php')){
+    require_once(dirname(__DIR__).'/vendor/autoload.php');
+}elseif(file_exists('../../../autoload.php')){
+    // after composer installed autoload file path.
+     require_once('../../../autoload.php');
+}
